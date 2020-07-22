ee4869209a99462f9b081d8eb039270804105015
evgeniymarianov <evgeniymarianov@gmail.com>
2020-07-22 16:02:30 +0300

    Template-lesson

--- hello_django/calc/views.py
+++ hello_django/calc/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views import View
+from django.template import Context, Template
 
-def calc_view(self, a, b):
-    return HttpResponse('{} + {} = {}'.format(str(a), str(b), str(a + b)))
+def index(request):
+    return HttpResponse('calc')
+
+def calc_view(request, a, b):
+    #return HttpResponse('{} + {} = {}'.format(str(a), str(b), str(sum)))
+    print(request)
+    if request.method == 'GET':
+        sum = a + b
+        context = {'a': a, 'b': b, 'sum': sum}
+        return render(request, 'calc/calc.html', context = context)
 
 class CalcView(View):
 
     def get(self, request, *args, **kwargs):
+        print(request)
         return HttpResponse('calc')
